c7963a3a42e56ac8a715bdbe37664dfd6105fffa
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-10-01 13:24:26 -0700

    Bugged

--- src/App.js
+++ src/App.js
@@ -1,75 +1,72 @@
 import React from 'react';
-import TodoList from './components/TodoComponents/TodoList';
+import Todo from './components/TodoComponents/Todo';
 import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ],
-      todo: ''
-    };
+      tasks: []
+    }
   }
+
+  // need method for adding task to state
+  // this will handle to onClick method from TodoForm
+  handleChanges = taskName => {
+    const newTask = {
+      task: taskName,
+      id: Date.now(),
+      completed: false
+    };
+    const taskList = this.state.tasks.slice();
+    taskList.push(newTask);
+    this.setState({ tasks: taskList });
+  };
+
+  handleComplete = taskID => {
+    this.setState({
+      tasks: this.state.tasks.map(rec =>
+        rec.id === taskID ? { ...rec, completed: !rec.completed } : rec
+      )
+    });
+  }
+
+    function clearComplete() {
+    props.toggleComplete(props.item.id);
+  }
+
+  return (
+    <div className={classNames} onClick={updatePurchaseHere}>
+      <p>{props.item.name}</p>
+    </div>
+  );
+
+ 
+
+
+
+
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  addTodo = e => {
-    e.preventDefault();
-    const newTodo = { task: this.state.todo, completed: false, id: Date.now() };
-    this.setState({ 
-      todos: [...this.state.todos, newTodo], 
-      todo: '' 
-    });
-  };
-
-  changeTodo = e => this.setState({ [e.target.name]: e.target.value });
-
-  toggleTodoComplete = id => {
-    let todos = this.state.todos.slice();
-    todos = todos.map(todo => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        return todo;
-      } else {
-        return todo;
-      }
-    });
-    this.setState({ todos });
-  };
-
-  clearCompletedTodos = e => {
-    e.preventDefault();
-    let todos = this.state.todos.filter(todo => !todo.completed);
-    this.setState({ todos });
-  };
-
-  render() {
+  render() 
     return (
       <div>
-        <TodoList
-          handleToggleComplete={this.toggleTodoComplete}
-          todos={this.state.todos}
-        />
-        <TodoForm
-          value={this.state.todo}
-          handleTodoChange={this.changeTodo}
-          handleAddTodo={this.addTodo}
-          handleClearTodos={this.clearCompletedTodos}
-        />
+        <h2>
+          <div className="todoListMain">
+            <div className="header">
+              <TodoForm addItem={this.handleChanges} />
+              <TodoList tasks={this.state.tasks} completeTask={this.handleComplete} />
+              <button onClick={this.removePurchased}>Clear Completed</button>
+            </div>
+          </div>
+
+        </h2>
+
       </div>
     );
-  }
-}
-
+  
 export default App;
+
